5bb01f190955c612865fb7008fd2decebb18dd3a
Casey <caseyderiso@gmail.com>
2020-12-26 21:45:01 -0600

    initialized database

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+process.env.JAWSBD_URL
+  ? (sequelize = new Sequelize(process.env.JAWSDB_URL))
+  : (sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+        port: 3306,
+      }
+    ));
+
+module.exports = sequelize;
